282adf6eff08fdeb711c745e7e60c1be7b498683
Mariam <mero23862@gmail.com>
2025-06-03 03:09:51 +0300

    app name & icon

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -25,7 +25,7 @@ class AppDrawer extends StatelessWidget {
             height: 166,
             child: Center(
               child: Text(
-                "News App",
+                "InBrief",
                 style: Theme
                     .of(context)
                     .textTheme
@@ -117,66 +117,6 @@ class AppDrawer extends StatelessWidget {
                                   .copyWith(color: Colors.white),
                             );
                           },
-                        ),
-                        Icon(
-                          Icons.arrow_drop_down,
-                          color: Colors.white,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(height: 24),
-                Divider(
-                  color: Colors.white,
-                  endIndent: 20,
-                  indent: 20,
-                ),
-                SizedBox(height: 24),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.language,
-                      color: Colors.white,
-                    ),
-                    SizedBox(width: 8),
-                    Text(
-                      "Language",
-                      style: Theme
-                          .of(context)
-                          .textTheme
-                          .titleMedium!
-                          .copyWith(color: Colors.white),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 8),
-                ElevatedButton(
-                  style: ButtonStyle(
-                    padding: WidgetStatePropertyAll(
-                      EdgeInsets.symmetric(vertical: 16),
-                    ),
-                    backgroundColor: WidgetStatePropertyAll(Colors.transparent),
-                    shape: WidgetStatePropertyAll(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16),
-                        side: BorderSide(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                  onPressed: () {},
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "English",
-                          style: Theme
-                              .of(context)
-                              .textTheme
-                              .titleMedium!
-                              .copyWith(color: Colors.white),
                         ),
                         Icon(
                           Icons.arrow_drop_down,
